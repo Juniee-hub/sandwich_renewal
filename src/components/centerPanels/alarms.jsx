5d4bd8933ef62ce3579d2b28774bd39f027b14b0
juniee <junes3754@gmail.com>
2022-05-14 15:37:33 +0900

    알림 패널 페이징 처리

--- src/components/centerPanels/alarms.jsx
+++ src/components/centerPanels/alarms.jsx
@@ -1,32 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { Button, Empty, Pagination }  from 'antd'
-import { Link }                       from 'react-router-dom'
+import React, { useEffect, useState }     from 'react'
+import { Button, Empty, Pagination }      from 'antd'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import queryString                        from 'query-string'
 
 import styles from './alarms.module.css'
 
 const Alarms = () => {
 
-    const [paging, setPaging] = useState({
-        // current: ParamHelper.parse(props.location.search).get('page') || 1,
-        current: 1,
-        defaultPageSize: 15,
-        showSizeChanger: false,
-        total: 947
-    })
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const getPage = () => {
+        const nowPage = queryString.parse(location.search)
+        return {
+            current: parseInt(nowPage.page || 1),
+            defaultPageSize: 15,
+            showSizeChanger: false,
+            total: 947,
+        }
+    }
+
+    const [paging, setPaging] = useState(getPage)
 
     useEffect(() => {
 
         // return () => {
         // }
-
     }, [])
 
     const handlePageChange = (page) => {
-
-        const pager = { ...paging, current: page }
-
-        console.log(pager)
-        setPaging(pager)
+        // const pager = { ...paging, current: page }
+        setPaging({ ...paging, current: page })
+        const href = `${location.pathname}?page=${page}`
+        navigate(href)
 
         // Todo 다시 통신 하여 데이터 수정
     }
@@ -72,36 +78,36 @@ const Alarms = () => {
                             <span className={styles.date}>2022년 5월 4일 (수) 오후 6:39</span>
                         </div>
 
-                            <Link to={'/app/project?channel=8&amp;project=206#/comment/3003'}>
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="13.299" height="12.783" viewBox="0 0 13.299 12.783">
-                                        <path d="M-6303,6039.8v12.283h12.8" transform="translate(6303.501 -6039.801)" fill="none" stroke="#e4e5e8" strokeWidth="1.5"/>
-                                    </svg>
-                                    <div className={styles.msgBox}>
-                                        <div>
-                                            <div className={styles.thumbnailWrap}>
-                                                <img className={styles.thumbnail} src="https://sandwich.so/api/v1/storage/thumb/440" alt={'썸네일'}/>
+                        <Link to={'/app/project?channel=8&amp;project=206#/comment/3003'}>
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="13.299" height="12.783" viewBox="0 0 13.299 12.783">
+                                    <path d="M-6303,6039.8v12.283h12.8" transform="translate(6303.501 -6039.801)" fill="none" stroke="#e4e5e8" strokeWidth="1.5"/>
+                                </svg>
+                                <div className={styles.msgBox}>
+                                    <div>
+                                        <div className={styles.thumbnailWrap}>
+                                            <img className={styles.thumbnail} src="https://sandwich.so/api/v1/storage/thumb/440" alt={'썸네일'}/>
+                                        </div>
+                                        <div className={styles.group}>
+                                            <div className={styles.infoWrap}>
+                                                <div className={styles.nickNameWrap}>
+                                                    <div className="msg">
+                                                        <span className={styles.nickName}>박지호</span><span className={styles.date}>2022년 5월 4일 (수)</span>
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className={styles.group}>
-                                                <div className={styles.infoWrap}>
-                                                    <div className={styles.nickNameWrap}>
-                                                        <div className="msg">
-                                                            <span className={styles.nickName}>박지호</span><span className={styles.date}>2022년 5월 4일 (수)</span>
-                                                        </div>
-                                                    </div>
+                                            <div className={styles.msgGroup}>
+                                                {/*<div className="msg ql-editor">*/}
+                                                <div className={`${styles.msg} ${styles.editor}`}>
+                                                    <div>검진내역에 검사결과서 링크만 있으면 타인도 로그인 없이 열람 가능합니다.http://clinic2022.planidev.com/mypage02/confirm_findings/id/156</div>
                                                 </div>
-                                                <div className={styles.msgGroup}>
-                                                    {/*<div className="msg ql-editor">*/}
-                                                    <div className={`${styles.msg} ${styles.editor}`}>
-                                                        <div>검진내역에 검사결과서 링크만 있으면 타인도 로그인 없이 열람 가능합니다.http://clinic2022.planidev.com/mypage02/confirm_findings/id/156</div>
-                                                    </div>
-                                                    <div className={styles.msgAppWrap}></div>
-                                                </div>
+                                                <div className={styles.msgAppWrap}></div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </Link>
+                            </div>
+                        </Link>
                     </div>
                 </li>
 
